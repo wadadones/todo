409ccccd5d870b93166bfdd23b4688ccfb621d3e
n.wada <jones3429@outlook.jp>
2019-10-02 13:27:25 +0900

    v-app-bar, v-content, right fixed, hidden-md-and-down, d-flex, flex-row

--- todo_front/src/App.vue
+++ todo_front/src/App.vue
@@ -3,36 +3,55 @@
   //- v-app
     //- router-view
     //- sidebar(@show-side-bar="showSideBar")
-  div#app
-    h2 My Todo
-    //- <!-- 絞り込みラジオボタン -->
-    label(v-for="label in options")
-      input(type="radio" v-model="current" :value="label.value")
-      span {{ label.label }}
+  v-app#app
+    v-app-bar(app dark elevate-on-scroll color="cyan")
+      v-app-bar-nav-icon
+      v-toolbar-title My Todo
+      //- right fixedはいっしょに使用
+      v-btn(right fixed class="hidden-lg-and-down") xl
+      v-btn(right fixed class="hidden-md-and-down hidden-xl-only") lg
+      v-btn(right fixed class="hidden-sm-and-down hidden-lg-and-up") md
+      v-btn(right fixed class="hidden-xs-only hidden-md-and-up") sm
+      v-btn(right fixed class="hidden-sm-and-up") xs
 
-    //- <!-- テーブルヘッダー -->
-    table
-      thead
-        tr
-          th.id ID
-          th.comment comment
-          th.state state
-          th.button -
-      tbody
-        tr(v-for="todo in computedTodos" :key="todo.id")
-          th {{ todo.id }}
-          td {{ todo.comment }}
-          td.state
-            button(@click="doChangeState(todo)") {{ labels[todo.state] }}
-          td.button
-            button(@click="doRemove(todo)") delete
-    //- <!-- ToDo テーブル -->
-    h2 new task
-    form.add-form(@submit.prevent="doAdd")
-      p comment
-      input(type="text" ref="comment")
-      button(type="submit") add
-    //- 新規登録フォーム
+    //- <!-- 絞り込みラジオボタン -->
+    //- v-contentでtoolbarとのマージンをとる
+    v-content
+      v-container
+        //- v-layout(row wrap)
+        //-   v-flex()
+        v-card(class="d-flex flex-row", color="grey lighten-2", flat, tile)
+          v-card(outlined tile class="hidden-md-and-down") おおお
+          v-card(outlined tile) おおお
+
+
+        label(v-for="label in options")
+          input(type="radio" v-model="current" :value="label.value")
+          span {{ label.label }}
+
+        //- <!-- テーブルヘッダー -->
+        table(ma-auto)
+          thead
+            tr
+              th.id ID
+              th.comment comment
+              th.state state
+              th.button -
+          tbody
+            tr(v-for="todo in computedTodos" :key="todo.id")
+              th {{ todo.id }}
+              td {{ todo.comment }}
+              td.state
+                button(@click="doChangeState(todo)") {{ labels[todo.state] }}
+              td.button
+                button(@click="doRemove(todo)") delete
+        //- <!-- ToDo テーブル -->
+        h2 new task
+        form.add-form(@submit.prevent="doAdd")
+          p comment
+          input(type="text" ref="comment")
+          button(type="submit") add
+        //- 新規登録フォーム
 </template>
 
 
