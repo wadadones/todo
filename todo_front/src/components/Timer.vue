07eefa1369226a82c9f2dba021c460845468daca
n.wada <jones3429@outlook.jp>
2019-10-03 12:04:34 +0900

    watcherでタイマーの色を変える

--- todo_front/src/components/Timer.vue
+++ todo_front/src/components/Timer.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   v-layout
     v-flex(xs12).text-center
-      v-card.font-weight-light.pa-2(@click="$emit('start')")
+      v-card.font-weight-light.pa-2(@click="$emit('start')" :color="color" :dark="dark")
         | {{ timer }}
 
 </template>
@@ -9,6 +9,26 @@
 <script>
 export default {
   name: "Timer",
-  props: ["timer", "state"]
+  props: ["timer", "state"],
+  data() {
+    return {
+      color: "",
+      dark: false
+    }
+  },
+
+  watch: {
+    // 変わった後のstateがrunningだったら色を帰る
+    state(val) {
+      if(val == "running") {
+        this.color = "teal"
+        this.dark = true
+      }
+      else {
+        this.color = ""
+        this.dark = false
+      }
+    }
+  }
 }
 </script>
